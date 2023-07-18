34a5b5f5a25647deb83657693f1db1c61c9a8e5a
Ochibo-JJ <133512040+Ochibo-JJ@users.noreply.github.com>
2023-07-18 23:12:54 +0300

    calculation of hypotenuse using visual studio

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // JavaScript source code
+	let base;
+	let height;
+	let hypotenuse;
+
+	document.getElementById("submitButton").onclick = function(){
+		base = document.getElementById("aTextBox").value;
+		base = Number(base);
+
+		height = document.getElementById("bTextBox").value;
+		height = Number(height);
+
+		hypotenuse = Math.sqrt(Math.pow(base, 2) + Math.pow(height, 2))
+		document.getElementById("cLabel").innerHTML = "Hypotenuse is: " + hypotenuse;
+	}
